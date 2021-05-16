005b49b929da26d522a7d9d76901c840a5bce6ab
Kiran C Nayak <33034799+KiranCNayak@users.noreply.github.com>
2021-05-16 19:11:20 +0530

    Added the change to go inside the project folder by default, test 2

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ if (!errorInFileName) {
       console.log('Next Step - Run the app')
       console.log('')
       console.log('npm run dev')
-      return runCommand('cd', [`${name}`], {
+      return runCommand('cd', [`${name}/`], {
         cwd: `${process.cwd()}/${name}`,
       })
     })
